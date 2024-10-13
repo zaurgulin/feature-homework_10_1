90a9f13a27ecfdaeaf93a65580eb869c72eb1921
Zaur Guin <zaur_zzz@mail.ru>
2024-10-13 16:12:07 +0300

    message1

---
+++ src/masks.py
@@ -0,0 +1,18 @@
+def get_mask_card_number(card_number: str) -> str | None:
+    """Функция маскировки номера карты"""
+    if card_number.isdigit() and len(card_number) == 16:
+        return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
+    else:
+        return None
+
+
+def get_mask_account(ass_number: str) -> str | None:
+    """Функция маскировки номера счета"""
+    if ass_number.isdigit() and len(ass_number) == 20:
+        return f"{"*" * 2}{ass_number[-4::]}"
+    else:
+        return None
+
+
+#print(get_mask_card_number("7000792289606361"))
+#print(get_mask_account("73654108430135874305"))
---
+++ src/widget.py
@@ -0,0 +1,24 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(nums: str) -> str | None:
+    """Функция общей маскировки корты и счета"""
+    if "Счет" in nums:
+        return get_mask_account(nums)
+    else:
+        cards = get_mask_card_number(nums[-16:])
+        new_card = nums. replace(nums[-16:], cards)
+        return new_card
+
+
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Maestro 7000792289606361"))
+
+
+def get_date(date: str) -> str | None:
+    """Функция преобразования даты"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+
+
+print(get_date("2024-10-13T02:26:18.671407"))
