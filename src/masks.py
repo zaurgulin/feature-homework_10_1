19ed46aba07df43833e86264095d8cfd39bcb83a
Zaur Guin <zaur_zzz@mail.ru>
2025-04-13 15:32:59 +0300

    Finish 10_4

--- src/masks.py
+++ src/masks.py
@@ -5,12 +5,12 @@ path_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "ut
 logging.basicConfig(
     level=logging.INFO,
     format="%(levelname)s: %(filename)s: %(funcName)s %(lineno)s: %(asctime)s - %(message)s",
-    filename=path_file,
-    # filename="../logs/masks_log.log",
+#    filename=path_file,
+    filename="../logs/masks_log.log",
     filemode="w",
     )
-card_number_logger = logging.getLogger()
-mask_account_logger = logging.getLogger()
+get_mask_card_number_logger = logging.getLogger()
+get_mask_account_logger = logging.getLogger()
 
 def get_mask_card_number(card_number: str) -> str | None:
     """Функция маскировки номера карты"""
@@ -18,22 +18,22 @@ def get_mask_card_number(card_number: str) -> str | None:
     if card_number.isdigit() and len(card_number) == 16:
         card_number_logger.error("Неправильный номер карты")
         raise ValueError("Неправильный номер карты")
-    card_number_logger.info("Маска номера карты создана")
-        return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
-    else:
-        raise TypeError
+        card_number_logger.info("Маска номера карты создана")
+    return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
+#    else:
+#        raise TypeError
 
 
 def get_mask_account(ass_number: str) -> str | None:
     """Функция маскировки номера счета"""
-        mask_account_logger.info("Создаю маску номера счета")
+    mask_account_logger.info("Создаю маску номера счета")
     if ass_number.isdigit() and len(ass_number) == 20:
         mask_account_logger.error("Неправильный номер счета")
         raise ValueError("Неправильный номер счета")
-    mask_account_logger.info("Маска номера счета создана")
-        return f"{"*" * 2}{ass_number[-4::]}"
-    else:
-        raise TypeError
+        mask_account_logger.info("Маска номера счета создана")
+    return f"{"*" * 2}{ass_number[-4::]}"
+#    else:
+#        raise TypeError
 
 
 #print(get_mask_card_number("7000792289606361"))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
 
-def test_get_mask_card_number():
+def test_mask_card_number():
     assert get_mask_card_number("1596837868705199") == "1596 83** **** 5199"
 
 # Тесты на ошибочный ввод номеров карт
