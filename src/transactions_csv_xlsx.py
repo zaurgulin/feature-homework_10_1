fdd256c9409c2f5909c2955adba3a96daa26b9fb
Zaur Guin <zaur_zzz@mail.ru>
2025-04-22 15:49:17 +0300

    Finish 10_7

--- src/transactions_csv_xlsx.py
+++ src/transactions_csv_xlsx.py
@@ -1,30 +1,39 @@
 import csv
-
 import pandas as pd
 
 
-def transactions_csv(filename: str) -> list:
-    """ Считывает CSV файл с транзакциями и возвращает список словарей этих транзакций """
-    if len(filename) == 0 or not isinstance(filename, str):
-        return []
+def transactions_csv(path_to_the_file: str) -> list:
+    """Функция, для считывания финансовых операций из CSV принимает путь к файлу CSV
+    и возвращает список словарей с транзакциями"""
     try:
-        with open(filename, encoding="utf-8") as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=";")
-            return list(reader)
+        with open(path_to_the_file, encoding="utf-8") as file:
+            pd.read_csv(file)
+    except ValueError:
+        return []
     except FileNotFoundError:
         return []
+    else:
+        with open(path_to_the_file, encoding="utf-8") as file:
+            operations = pd.read_csv(file, delimiter=";")
+            return operations.to_dict(orient="records")
 
 
-def transactions_xlsx(filename: str) -> list:
-    """ Считывает Excel файл с транзакциями и возвращает список словарей этих транзакций """
-    if len(filename) == 0 or not isinstance(filename, str):
-        return []
+def transactions_xlsx(path_to_the_file: str) -> list:
+    """Функция, для считывания финансовых операций из EXCEL принимает путь к файлу EXCEL в качестве аргумента
+    и возвращает список словарей с транзакциями"""
     try:
-        ecxel_data = pd.read_excel(filename)
-        ecxel_data = ecxel_data.to_dict("records")
-        return ecxel_data
+        pd.read_excel(path_to_the_file)
+    except ValueError:
+        return []
     except FileNotFoundError:
         return []
+    else:
+        operations = pd.read_excel(path_to_the_file)
+        return operations.to_dict(orient="records")
+
+
+# print(transactions_csv('../data/transactions.csv'))
+# print(transactions_xlsx('../data/transactions_excel.xlsx'))
 
 
 #print(transactions_csv("С:/PycharmProjects/pythonProject7/data/transactions.csv"))
--- tests/test_transactions_csv_excel.py
+++ tests/test_transactions_csv_excel.py
@@ -10,8 +10,8 @@ def test_transactions_csv_correct_file():
     """ Имитирует успешное чтение CSV-файла и проверяет, что результат соответствует ожидаемому списку словарей. """
     mock_csv_content = "name;amount;date\nJohn;100;2023-01-01\nJane;150;2023-01-02\n"
     expected_result = [
-        {"name": "John", "amount": "100", "date": "2023-01-01"},
-        {"name": "Jane", "amount": "150", "date": "2023-01-02"},
+        {"name": "John", "amount": 100, "date": "2023-01-01"},
+        {"name": "Jane", "amount": 150, "date": "2023-01-02"},
     ]
 
     with patch("builtins.open", mock_open(read_data=mock_csv_content), create=True):
